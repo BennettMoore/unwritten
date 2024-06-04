9ecfc477deb1e8094411aa5939bca74999621eb8
BennettMoore <bennettwilliammoore@gmail.com>
2024-06-04 15:08:13 -0400

    Skill nodes now have a heirarchy, and dynamically draw lines connecting parent/child nodes

--- drop_point.gd
+++ drop_point.gd
@@ -6,29 +6,61 @@ class_name DropPoint
 ## Handles drag/drop logic and skill data transfer
 ## @author: Bennett Moore 2024
 
+signal skills_changed(in_tree:bool, id:int, data:SkillData)
+
 var _skill_data = preload("res://skill_data.gd")
 
 @export var empty_texture:CompressedTexture2D = load("res://icon.svg") ## The default texture to show when no skill is socketed
 @export var held_data:SkillData ## The resource containing all skill data
 @export var has_skill = false ## Whether the skill in this drop point is valid or not
+@export var in_tree = true ## Whether this drop point is in the actual skill tree or not
+@export_range(-1, 100, 1) var id = -1 ## The unique ID of this node in the tree, or -1 if not in tree
+@export var children:Array[DropPoint]
+var parents:Array[DropPoint]
+var lines:Dictionary = {}
 func _ready():
+	# Find all your children and tell them who their parent(s) are
+	if children != null and !children.is_empty():
+		for child in children:
+			child.parents.append(self)
+	
+	# TODO Remove this code and get skills functioning correctly
 	if held_data == null and has_skill:
 		print("Found null data!")
-		held_data = SkillData.new([load("res://art/skill_empty_1.png"), load("res://art/skill_empty_2.png"), load("res://art/skill_empty_3.png"), load("res://art/skill_empty_4.png")], "Test", 0)
-		texture = load("res://art/skill_empty_1.png")
+		held_data = SkillData.new(
+			[load("res://art/skill_duck_1.png"), load("res://art/skill_duck_2.png"), load("res://art/skill_duck_3.png"), load("res://art/skill_duck_4.png")],
+			"Test",
+			min(id+1, 3),
+			false,
+			"This is a tooltip!"
+		)
+		texture = held_data.skill_icons[held_data.skill_level]
 	elif has_skill:
 		print("Added Skill \""+held_data.skill_name+"\"!")
 		texture = held_data.skill_icons[held_data.skill_level]
 		has_skill = true
 	
 func _draw():
+	if children != null and !children.is_empty() and lines.size() != children.size():
+		for child in children:
+			if lines.find_key(child):
+				continue
+			else:
+				var line = Line2D.new()
+				line.add_point(global_position + Vector2(size.x/2, size.y/2))
+				line.add_point(child.global_position + Vector2(child.size.x/2, child.size.y/2))
+				line.top_level = true
+				add_child(line)
+				lines[child] = line
 	set_icon()
 
 func _get_drag_data(at_position):
 	var preview_texture = TextureRect.new()
 	preview_texture.texture = texture
 	preview_texture.expand_mode = 1
-	preview_texture.set_modulate(Color(1, 1, 1, 0.5))
+	preview_texture.set_modulate(Color(0.7, 0.7, 0.7, 0.9))
+	preview_texture.set_size(size)
+	preview_texture.z_index = 10
 	var preview = Control.new()
 	preview.add_child(preview_texture)
 	set_drag_preview(preview)
@@ -46,6 +78,7 @@ func _drop_data(at_position, data):
 	held_data = data
 	has_skill = true
 	set_icon()
+	skills_changed.emit(in_tree, id, held_data)
 
 func set_icon():
 	if held_data == null or held_data.skill_level > held_data.skill_icons.size():
--- skill_data.gd
+++ skill_data.gd
@@ -9,9 +9,11 @@ class_name SkillData
 @export var skill_name: String ## The name of the skill
 @export_range(0, 3) var skill_level: int ## What level the skill is (should always be within the bounts of skill_icons)
 @export var is_active: bool ## Whether the skill is active or passive
+@export var tooltip: String ## The skill's tooltip
 
-func _init(_skill_icons:Array[CompressedTexture2D], _skill_name = "N/A", _skill_level = 0, _is_active = false):
+func _init(_skill_icons:Array[CompressedTexture2D], _skill_name = "N/A", _skill_level = 0, _is_active = false, _tooltip = "No tooltip provided"):
 	skill_icons = _skill_icons
 	skill_name = _skill_name
 	skill_level = _skill_level
 	is_active = _is_active
+	tooltip = _tooltip
